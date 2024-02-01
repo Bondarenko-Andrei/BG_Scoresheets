d41d7e54732d8721d94d3472109db8b1eab951b5
Andrei Bondarenko <144313202+Bondarenko-Andrei@users.noreply.github.com>
2024-02-01 14:55:11 +0400

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = "475a505c450001887c4edbabc33662799231a11b90fc6392c0574e86af2f7cfa"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///games.db"
+    FLASK_ADMIN_SWATCH = "cerulean"
